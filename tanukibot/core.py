0fdc95003602f7b2bc677d6026f6e34bf5242696
Kent English <kent.english@gmail.com>
2018-11-06 09:25:19 -0500

    Moving fallback into core

--- tanukibot/core.py
+++ tanukibot/core.py
@@ -12,23 +12,26 @@ NON_ALPHA_REGEX = r'[^a-z\s]'
 MULTI_SPACE_REGEX = r'\s{2,}'
 
 class Core:
-    def __init__(self, corpus='tanuki.txt', stop_words=STOP_WORDS, stemmer_lang='english', sentence_min=50, sentence_max=250, initial_size=10000):
+    def __init__(self, corpus='tanuki.txt', stop_words=STOP_WORDS, stemmer_lang='english', sentence_min=50, sentence_max=250, initial_size=10000, new_line=True):
         self.corpus = corpus
         self.stop_words = stop_words
         self.stemmer = nltk.stem.SnowballStemmer(stemmer_lang)
         self.sentence_min = sentence_min
         self.sentence_max = sentence_max
+        self.new_line = new_line
         self.model = None
         self.sentences = {}
         self.lexicon = {}
 
-        self.load_model()
         self.generate_sentences(initial_size)
 
     def load_model(self):
         with open(self.corpus) as f:
             corpus = f.read()
-        self.model = markovify.NewlineText(corpus)
+        if self.new_line:
+            self.model = markovify.NewlineText(corpus)
+        else:
+            self.model = markovify.Text(corpus)
 
     def sentence_to_words(self, sentence):
         lowered = sentence.lower()
@@ -38,7 +41,11 @@ class Core:
         stemmed = [self.stemmer.stem(word) for word in words]
         return set(stemmed).difference(self.stop_words)
 
-    def generate_sentences(self, n=1000):
+    def generate_sentences(self, n=100):
+        print('Generating', n, 'sentences')
+        old_total = len(self.sentences.keys())
+
+        self.load_model()
         for i in range(n):
             sentence = self.model.make_short_sentence(self.sentence_max, self.sentence_min)
             if not sentence:
@@ -54,6 +61,8 @@ class Core:
                 if word not in self.lexicon:
                     self.lexicon[word] = set()
                 self.lexicon[word].add(key)
+        new_total = len(self.sentences.keys())
+        print('Added', new_total - old_total, 'sentences from', self.corpus)
 
     def get_sentence(self, prompt):
         words = self.sentence_to_words(prompt)
@@ -69,8 +78,14 @@ class Core:
                 continue
             sentence = self.sentences[key].compare(sentence)
         if not sentence:
-            print('No match found')
-            return None
+            return self.fallback()
         else:
             sentence.last_used = dt.datetime.now()
             return sentence.text
+
+    def fallback(self):
+        sentence = self.model.make_short_sentence(self.sentence_max, self.sentence_min)
+        if not sentence:
+            sentence = ''
+        return '¯\_(ツ)_/¯ ' + sentence
+
